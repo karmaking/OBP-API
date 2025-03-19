8abe95f276d2ee5a48feb0908912a07e10dac3de
Marko Milić <marko@tesobe.com>
2025-03-19 14:07:35 +0100

    feature/Check mandatory headers of Berlin Group

--- obp-api/src/main/scala/code/api/util/APIUtil.scala
+++ obp-api/src/main/scala/code/api/util/APIUtil.scala
@@ -3244,8 +3244,8 @@ object APIUtil extends MdcLoggable with CustomJsonFormats{
       val verb = result._2.map(_.verb).getOrElse("None")
       val body = result._2.flatMap(_.httpBody)
       val reqHeaders = result._2.map(_.requestHeaders).getOrElse(Nil)
-      // Verify signed request (Berlin Group)
-      BerlinGroupSigning.verifySignedRequest(body, verb, url, reqHeaders, result)
+      // Berlin Group checks
+      BerlinGroupCheck.validate(body, verb, url, reqHeaders, result)
     } map {
       result =>
         val excludeFunctions = getPropsValue("rate_limiting.exclude_endpoints", "root").split(",").toList
@@ -3300,8 +3300,8 @@ object APIUtil extends MdcLoggable with CustomJsonFormats{
       val verb = result._2.map(_.verb).getOrElse("None")
       val body = result._2.flatMap(_.httpBody)
       val reqHeaders = result._2.map(_.requestHeaders).getOrElse(Nil)
-      // Verify signed request if need be
-      BerlinGroupSigning.verifySignedRequest(body, verb, url, reqHeaders, result)
+      // Berlin Group checks
+      BerlinGroupCheck.validate(body, verb, url, reqHeaders, result)
     } map { result =>
       result._1 match {
         case Empty if result._2.flatMap(_.consumer).isDefined => // There is no error and Consumer is defined
