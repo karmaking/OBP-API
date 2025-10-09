609549ec616cb5aeecb994ae963499f8efb4006c
simonredfern <simon@tesobe.com>
2025-10-09 12:16:31 +0200

    refactor: OIDC three scripts

--- obp-api/src/main/scripts/sql/OIDC/give_admin_access.sql
+++ obp-api/src/main/scripts/sql/OIDC/give_admin_access.sql
@@ -1,21 +1,9 @@
--- =============================================================================
--- GIVE READ ACCESS TO OBP USERS AND WRITE ACCESS TO OBP CONSUMERS
--- =============================================================================
--- This orchestration script grants OIDC_ADMIN_USER read access to user-related views
--- and full CRUD access to consumer/client management
--- by including the necessary component scripts
-
 -- Include variable definitions and database connection
 \i set_and_connect.sql
 
--- Create the OIDC users
--- TODO check if we need both here.
-\i cre_OIDC_USER.sql
+
 \i cre_OIDC_ADMIN_USER.sql
 
--- Create all three views (which include the necessary GRANT statements)
-\i cre_v_oidc_users.sql
-\i cre_v_oidc_clients.sql
 \i cre_v_oidc_admin_clients.sql
 
 \echo 'Bye from give_read_access_to_obp_users_and_write_access_to_obp_consumers.sql'
--- obp-api/src/main/scripts/sql/OIDC/give_read_access_to_clients.sql
+++ obp-api/src/main/scripts/sql/OIDC/give_read_access_to_clients.sql
@@ -1,11 +1,9 @@
-
 -- Include variable definitions and database connection
 \i set_and_connect.sql
 
--- Create the OIDC user if it doesn't exist
-\i cre_OIDC_USER.sql
+-- Note we don't create the OIDC_USER here
 
 -- Create the v_oidc_users view (which includes GRANT SELECT to OIDC_USER)
-\i cre_v_oidc_users.sql
+\i cre_v_oidc_clients.sql
 
-\echo 'Bye from give_read_access_to_obp_users.sql'
+\echo 'Bye from give_read_access_to_obp_clients.sql'
