c1585913ed6f197ba5a6a75b69e8101a066be06a
Marko Milić <marko@tesobe.com>
2025-11-05 14:09:48 +0100

    feature/Revert rate limiting v3.1.0 and v4.0.0 paths

--- obp-api/src/test/scala/code/api/v5_1_0/V510ServerSetup.scala
+++ obp-api/src/test/scala/code/api/v5_1_0/V510ServerSetup.scala
@@ -38,7 +38,7 @@ trait V510ServerSetup extends ServerSetupWithTestData with DefaultUsers {
     val Some((c, _)) = consumerAndToken
     val consumerId = Consumers.consumers.vend.getConsumerByConsumerKey(c.key).map(_.consumerId.get).getOrElse("")
     Entitlement.entitlement.vend.addEntitlement("", resourceUser1.userId, ApiRole.CanUpdateRateLimits.toString)
-    val request400 = (v4_0_0_Request / "management" / "consumers" / consumerId / "consumer" / "rate-limits").PUT <@ (consumerAndToken)
+    val request400 = (v4_0_0_Request / "management" / "consumers" / consumerId / "consumer" / "call-limits").PUT <@ (consumerAndToken)
     makePutRequest(request400, write(putJson))
   }
 
